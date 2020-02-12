6f5aa4785b7047a8411d9fd2ae27ac48d8ea6fb9
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2020-02-12 11:45:15 +0100

    Addd staging and production config urls

--- kyc/config_staging.py
+++ kyc/config_staging.py
@@ -4,4 +4,4 @@ EMAIL_ADDRESS = "info@openkyc.live"
 HEX_SEED = os.environ['SEED']
 PASSWORD =  os.environ['PASSWORD']
 SUBJECT = "Verify your email address"
-REDIRECT_URL = "http://192.168.1.184:8081/verifyemail"
+REDIRECT_URL = "https://login.staging.jimber.org/verifyemail"
