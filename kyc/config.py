d8cdb9519a42ab879bffbaf2d3bb6fac540f473e
Lennert Defauw <lennert.defauw@jimber.org>
2023-03-14 10:04:20 +0100

    update email client

--- kyc/config.py
+++ kyc/config.py
@@ -1,6 +1,6 @@
 import os
 
-EMAIL_ADDRESS = "info@openkyc.live"
+EMAIL_ADDRESS = "tfconnect@threefold.io"
 HEX_SEED = os.environ['SEED']
 PASSWORD =  os.environ['PASSWORD']
 SUBJECT = "Verify your email address"
